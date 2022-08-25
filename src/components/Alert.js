9f31e0a495444f9bf6ef5ff0ed7fa48fb4ac7ca6
Mark Brodersen <U2405963@rts-365.dk>
2022-08-25 10:38:49 +0200

    Styling

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,11 +7,13 @@ const Alert = ({ setIsHidden }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       onClick={() => setIsHidden(false)}
-      className="z-10 fixed flex justify-center items-center w-screen h-screen bg-slate-900 bg-opacity-10"
+      className="z-10 fixed flex justify-center items-center w-screen h-screen bg-gray-900 bg-opacity-20"
     >
-      <div className="z-20 flex justify-center items-center bg-slate-200  w-2/3 h-1/3">
-        <h1>Hello, world!</h1>
-        filer
+      <div className="z-20 flex flex-col justify-center items-center bg-white rounded-3xl shadow-xl w-2/3 h-1/3">
+        <h1 className=" mt-24">Hello, world!</h1>
+        <button className="bg-gray-100 mt-auto mb-8 w-32 px-4 py-2 rounded-lg">
+          Ok
+        </button>
       </div>
     </motion.div>
   );
